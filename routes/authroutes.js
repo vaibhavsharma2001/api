58182ee05ed637bb32929405abeea12f06334f4e
vaibhavsharma2001 <136614875+vaibhavsharma2001@users.noreply.github.com>
2023-12-11 15:28:54 +0530

    Update authroutes.js

--- routes/authroutes.js
+++ routes/authroutes.js
@@ -1,17 +1,22 @@
 // routes/auth.js
 const express = require('express');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 const passport = require('passport');
-const authlogs = require('../controllers/authlogin');
-const authregs = require('../controllers/authregister');
-
+const authloginControl= require('../controllers/authlogin');
+const authRegisterControl= require('../controllers/authregister');
+const bcrypt=require("bcrypt");
 // Route for user registration
 router.post('/register', [
     body('username').notEmpty().withMessage('Username is required'),
     body('password').notEmpty().withMessage('Password is required'),
-  ], authregs.authregister);
+  ], authRegisterControl.register);
 
 // Route for user login
-router.post('/login', authlogs.authlogin);
-
+router.post('/login',function(req, res){
+  authloginControl.login
+} );
+// app.post('/user/all', function(req, res){
+//   Controller.Create
+// });
 module.exports = router;
