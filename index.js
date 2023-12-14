8c7689ab18103ecbd537ec80a5ac517df1de65aa
vaibhavsharma2001 <136614875+vaibhavsharma2001@users.noreply.github.com>
2023-12-14 21:19:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const cors = require('cors');
-const passportjs=require('../middleware/authenticate');
+const passportjs=require('../middleware/passport');
 const app = express();
 const PORT =  process.env.PORT || 4000
 
