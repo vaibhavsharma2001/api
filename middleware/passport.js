8a8dde2b9fc8bcc8b2253afd96aa6ed8fd2aa3fe
vaibhavsharma2001 <136614875+vaibhavsharma2001@users.noreply.github.com>
2023-12-17 23:31:28 +0530

    Update passport.js

--- middleware/passport.js
+++ middleware/passport.js
@@ -1,9 +1,9 @@
-// passport.js
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
-const User = require('../models/user'); // 
+const User = require('../models/user'); // Replace with the correct path to your User model
+const bcrypt = require('bcrypt');
 
 const secretKey = process.env.JWT_SECRET || 'secret-key';
 
@@ -12,7 +12,20 @@ passport.use(new LocalStrategy({
   usernameField: 'username',
   passwordField: 'password',
 }, async (username, password, done) => {
- 
+  try {
+    // Find the user by username in your database
+    const user = await User.findOne({ username });
+
+    // If the user is not found or the password is incorrect
+    if (!user || !bcrypt.compareSync(password, user.password)) {
+      return done(null, false, { message: 'Incorrect username or password' });
+    }
+
+    // If the username and password are correct, return the user
+    return done(null, user);
+  } catch (error) {
+    return done(error);
+  }
 }));
 
 // JWT Strategy for token authentication
@@ -22,7 +35,20 @@ const jwtOptions = {
 };
 
 passport.use(new JwtStrategy(jwtOptions, async (payload, done) => {
- 
+  try {
+    // Find the user by ID in your database
+    const user = await User.findById(payload.sub);
+
+    // If the user is not found
+    if (!user) {
+      return done(null, false, { message: 'User not found' });
+    }
+
+    // If the user is found, return the user
+    return done(null, user);
+  } catch (error) {
+    return done(error);
+  }
 }));
 
 module.exports = passport;
